ebaa2d3c408b428ff5df38d390ba757c9e264d68
Konstantin <konstantin.muenster@mail.com>
2022-10-21 06:56:35 +0200

    fix: remove compats

--- src/next.ts
+++ src/next.ts
@@ -1,14 +1,8 @@
 import globals from 'globals';
-import { FlatCompat } from '@eslint/eslintrc';
 
 import { ReactTypescriptEslintConfig } from './react';
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 export const NextEslintConfig = [
-  compat.extends('next/core-web-vitals'),
   ...ReactTypescriptEslintConfig,
   {
     files: ['**/*.tsx'],
--- src/react.ts
+++ src/react.ts
@@ -1,19 +1,9 @@
 import typescriptParser from '@typescript-eslint/parser';
 import globals from 'globals';
-import { FlatCompat } from '@eslint/eslintrc';
 
 import { TypescriptEslintConfig } from './typescript';
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 export const ReactTypescriptEslintConfig = [
-  compat.extends(
-    'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:react-hooks/recommended'
-  ),
   ...TypescriptEslintConfig,
   {
     files: ['**/*.tsx'],
